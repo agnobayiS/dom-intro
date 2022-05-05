0faee2ef1c6bd4d82e98dc722312df8e24a61f3e
agnobayiS <C.ya@live.com>
2022-05-05 14:32:21 +0200

    second commit

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,27 +1,40 @@
 // get a reference to the sms or call radio buttons
-const billItemTypeElement2 = document.querySelector(".billItemTypeWithSettings");
-const addBtnElement2 = document.querySelector(".radioBillAddBtn1");
-const BtnElement2 = document.querySelector(".updateSettings");
-const callsTotalElem2 = document.querySelector(".callTotalSettings");
-const smsTotalElem2 = document.querySelector(".smsTotalSettings");
-const totalCostElem2 = document.querySelector(".totalSettings");
+
+// const BtnElement2 = document.querySelector(".updateSettings");
+
 // get refences to all the settings fields
 //get a reference to the add button
-const callCost = document.querySelector(".callCostSetting");
-const smsCost = document.querySelector(".smsCostSetting");
-const warningLevel = document.querySelector(".warningLevelSetting");
-const criticalLevel = document.querySelector(".criticalLevelSetting");
+// const callCost = document.querySelector(".callCostSetting");
+// const smsCost = document.querySelector(".smsCostSetting");
+// const warningLevel = document.querySelector(".warningLevelSetting");
+// const criticalLevel = document.querySelector(".criticalLevelSetting");
 //get a reference to the 'Update settings' button
+var callCost1 = 0;
+var smsCost1 = 0;
+var criticalLevel1 = 0;
+var warningLevel1 = 0;
+
+
+
 
 // create a variables that will keep track of all the settings
-let total = 0;
-let totalCall = 0;
-let totalSms = 0;
+var total = 0;
+var totalCall = 0;
+var totalSms = 0;
 // create a variables that will keep track of all three totals.
 
 //add an event listener for when the 'Update settings' button is pressed
+//const billItemTypeElement2 = document.querySelector(".billItemTypeWithSettings");
+//const addBtnElement2 = document.querySelector(".radioBillAddBtn1");
+//const callsTotalElem2 = document.querySelector(".callTotalSettings");
+//const smsTotalElem2 = document.querySelector(".smsTotalSettings");
+// const totalCostElem2 = document.querySelector(".totalSettings");
+
+//addBtnElement2.addEventListener('click', textBillTotal);
+
 
 function textBillTotal() {
+    //alert(totalCall)
     // get the value entered in the billType textfield
 
     var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
@@ -29,34 +42,70 @@ function textBillTotal() {
         var billItemTypeWithSettings = checkedRadioBtn.value
         // billItemType will be 'call' or 'sms'
         // update the correct total
-         if (billItemTypeWithSettings === "call") {
-            totalCall += callCost
+
+        if (billItemTypeWithSettings === "call") {
+            if (criticalLevel1 > total) {
+                totalCall += callCost1;
+            }
         }
         else if (billItemTypeWithSettings === "sms") {
-            totalSms += smsCost;
+            if (criticalLevel1 > total) {
+                totalSms += smsCost1;
+            }
+
         }
     }
-        
+
     //update the totals that is displayed on the screen.
-    callsTotalElem2.innerHTML = totalCall.toFixed(2);
-    smsTotalElem1.innerHTML = totalSms.toFixed(2);
+    document.getElementById("callTotalSettings").innerHTML = totalCall.toFixed(2);
+    document.getElementById("smsTotalSettings").innerHTML = totalSms.toFixed(2);
     total = totalCall + totalSms;
-    totalCostElem2.innerHTML = total.toFixed(2);
+    document.getElementById("totalSettings").innerHTML = total.toFixed(2);
+
+    colourChange()
+    
+
+    
+
+}
 
 
-    if (total >= warningLevel) {
-        // adding the danger class will make the text red
-        totalCostElem2.classList.add("danger");
+function Update() {
+    //alert("Hello")
+    callCost1 = Number(document.getElementById("callCost").value);
+    smsCost1 = Number(document.getElementById("smsCost").value);
+    warningLevel1 = Number(document.getElementById("warn").value);
+    criticalLevel1 = Number(document.getElementById("critical").value);
+
+    //  callCost1 = Number(callCost.value)
+    //     smsCost1 = Number(smsCost.value)
+    //     criticalLevel1 = Number(criticalLevel.value)
+    //     warningLevel1 = Number(warningLevel.value)
+
+    colourChange()
+}
+function colourChange(){
+
+    if (total >= criticalLevel1) {
+        document.getElementById("totalSettings").classList.add("danger");
+
+
     }
-    else if (total >= criticalLevel) {
-        totalCostElem2.classList.add("warning");
+    else if (total >= warningLevel1) {
+        // adding the danger class will make the text red
+        document.getElementById("totalSettings").classList.add("warning");
+    }
+
+    if(criticalLevel1 >total &&  warningLevel1 > total ){
+        document.getElementById("totalSettings").classList.remove("danger");
+        document.getElementById("totalSettings").classList.remove("warning");
     }
 
 }
 
-addBtnElement2.addEventListener('click', textBillTotal);
 
-BtnElement2.addEventListener('click', textBillTotal);
+
+
 //add an event listener for when the add button is pressed
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
@@ -65,3 +114,4 @@ BtnElement2.addEventListener('click', textBillTotal);
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
+
